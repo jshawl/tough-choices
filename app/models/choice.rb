cc8353a4f6535f71ce81f53020383cc67e5d55b7
Jesse Shawl <jesse@jshawl.com>
2014-09-09 19:49:41 -0400

    show verdict

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -4,4 +4,9 @@ class Choice < ActiveRecord::Base
   def days_till_answer
     puts self.ttl
   end
+
+  def verdict
+    string = self.answers.where( answer: "yes" ).length.to_f / self.answers.length.to_f > 0.5 ? "yes" : "no"
+    "The answer is " + string + "! " 
+  end
 end
